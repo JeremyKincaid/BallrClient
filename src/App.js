7711d2f9636c60d453baece5a47dad251f58375f
ERika-tech257 <ErikaJtech@gmail.com>
2020-11-17 19:49:26 -0500

    Profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import UserProfile from './components/UserProfile/UserProfile';
 
 function App() {
   return (
     <div className="App">
-
+<UserProfile />
     </div>
   );
 }
