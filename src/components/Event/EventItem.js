b2d164f0d95d6b9f62a8372cbb7a3d7b2137605c
Jeremy D. Kincaid <jeremydkincaid@gmail.com>
2020-11-17 21:05:42 -0500

    minor changes to the Event Item in preparation for event sign up

--- src/components/Event/EventItem.js
+++ src/components/Event/EventItem.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './EventItem.css';
+import { Button } from 'reactstrap';
+
 
 const EventItem = (props) => {
 
@@ -19,9 +21,9 @@ const EventItem = (props) => {
             console.log("displayname:" + props.ev.createdById +rObj.user.displayname)  
             setHostName(rObj.user.displayname)})
     }
-
-    return(
-        <div className="ev-card">
+    if (props.ev.createdById === props.currentUser) {
+        return(
+            <div className="ev-card">
             <h3>{props.ev.name}</h3>
             <p>Sport: {props.ev.sport}.</p>
             <p>Location: {props.ev.location}</p>
@@ -30,8 +32,26 @@ const EventItem = (props) => {
             <p>Ends: {props.ev.endTime}</p>
             <p>{props.ev.currentPlayers} out of {props.ev.maxPlayers}</p>
             <p>Host: {hostName}</p>
+            
+            <Button className="secondary-button">Edit</Button>{' '}
         </div>
-    )
+        )
+    } else {
+        return(
+            <div className="ev-card">
+                <h3>{props.ev.name}</h3>
+                <p>Sport: {props.ev.sport}.</p>
+                <p>Location: {props.ev.location}</p>
+                <p>Date: {props.ev.date}</p>
+                <p>Starts: {props.ev.startTime}</p>
+                <p>Ends: {props.ev.endTime}</p>
+                <p>{props.ev.currentPlayers} out of {props.ev.maxPlayers}</p>
+                <p>Host: {hostName}</p>
+                
+                <Button className="secondary-button">Sign up</Button>{' '}
+            </div>
+        )
+    }
 }
 
 export default EventItem;
