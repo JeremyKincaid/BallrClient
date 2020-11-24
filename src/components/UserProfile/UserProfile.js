81ccdc0c8b28e413fae010b37e553a1a5462a5c6
ERika-tech257 <ErikaJtech@gmail.com>
2020-11-23 19:09:05 -0500

    Users are now  able to put their own info for rating and description

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -5,7 +5,6 @@ import {
   CardTitle, CardSubtitle
 } from 'reactstrap';
 import ImgUpload from './ImgUpload';
-import AddFriend from './AddFriend';
 
 
 // User Profile need displayname of users?
@@ -16,13 +15,6 @@ const UserProfile = (props) => {
   const [displayName, setDisplayName] = useState('')
   const [rating, setRating] = useState('')
   const [description, setDescription] = useState('')
-
-
-  // const newCard = () => {
-  //   setDisplayName(false)
-  //   setRating(true)
-  //   setDescription(false)
-  // }
 
   useEffect(
     () => {
@@ -36,42 +28,25 @@ const UserProfile = (props) => {
     console.log(json)
     setDisplayName(json.user.displayname)
 
-    setRating(json.rating)
+    setRating(json.user.rating)
 
-    setDescription(json.description)
-
-  
-    // fetch(`http://localhost:3000/user/${LID}`, {
-    //   method: 'GET'
-    // }).then (r => r.json())
-    // .then( rArr =>{
-
-    // console.log(rArr)
-
-    // if(rArr !== undefined){
-
-    // }
-
-
-
+    setDescription(json.user.description)
   }
-
-
 
 return (
   <div className="userProfile">
     <Card>
       <CardBody>
         <CardTitle tag="h3">BALLR</CardTitle>
-
         <CardSubtitle tag="h5" className="mb-2 text-muted">{`DisplayName:${displayName}`}</CardSubtitle>
         <ImgUpload sessionToken={props.sessionToken} />
         <br />
-        <CardText>"Write something amazing about yourself"</CardText>
-
-        <AddFriend />
+        <CardText>{`Here's a little about myself: ${description}`}</CardText>
+        <p>{`Rating: ${rating}`}</p>
+        
       </CardBody>
     </Card>
+
   </div>
 ) 
         }
